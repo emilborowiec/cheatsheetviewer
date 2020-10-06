15bf45294243ed7cb6f415d831e8cd8a2c277d06
Emil Borowiec <emil@ponderingprogrammer.com>
2020-10-06 20:07:47 +0200

    Moved to dedicated repo

---
+++ QuickSheet/PonderingProgrammer.QuickSheet/Model/CheatSheet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PonderingProgrammer.QuickSheet.Model
+{
+    public class CheatSheet
+    {
+        public CheatSheet(string title)
+        {
+            Title = title;
+        }
+
+        public string Title { get; }
+        public List<Section> Sections { get; } = new List<Section>();
+        public List<Cheat> Cheats { get; } = new List<Cheat>();
+    }
+}
---
+++ QuickSheet/PonderingProgrammer.QuickSheet/Model/Section.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PonderingProgrammer.QuickSheet.Model
+{
+    public class Section
+    {
+        public Section(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+        public List<Cheat> Cheats { get; } = new List<Cheat>();
+    }
+}
